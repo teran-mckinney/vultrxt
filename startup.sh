38b1895d11aa6bdc9044504f8bd04e67119f3306
Teran McKinney <sega01@go-beyond.org>
2015-11-10 04:45:00 +0000

    This may be working now

--- startup.sh
+++ startup.sh
@@ -1,21 +1,33 @@
 #!/bin/sh
 
-TAG=v0.11C
+# This runs at the top of cloud-init. We don't even have SSHD running without
+# this.
 
-pkg update -qf
-pkg upgrade -qfy
+export ASSUME_ALWAYS_YES=yes
 
-pkg install -qfy pwgen autotools pkgconf gmake boost-libs openssl db48 git curl micro_httpd micro_inetd #FIXME TODO: Remove git
+# Just have a gig free without this.
+service growfs onestart
+
+# pkg isn't installed by default on vultr, but this will bootstrap it
+# with the above option of ASSUME_ALWAYS_YES=yes
+pkg upgrade
+
+pkg install ca_root_nss autotools pkgconf gmake boost-libs openssl db48 git micro_httpd micro_inetd #FIXME TODO: Remove git
 chmod 700 /root
 mkdir /root/.bitcoin
-echo "rpcpassword=$(pwgen -s 20 1)" > /root/.bitcoin/bitcoin.conf
+echo 'debug=net
+maxoutbound=32
+logtimemicros=1
+rpcuser=*
+rpcpassword=*' > /root/.bitcoin/bitcoin.conf
 
 mkdir bitcoinxt; cd bitcoinxt
 #FIXME This seems to have a 302 loop right now.
 #fetch -qo - https://github.com/bitcoinxt/bitcoinxt/archive/$TAG.tar.gz | tar xzf -
-git clone https://github.com/bitcoinxt/bitcoinxt.git
+
+# Using jtoomin's fortestnet branch.
+ git clone --depth 1 https://github.com/jtoomim/bitcoinxt.git -b fortestnet
 cd bitcoinxt
-git checkout $TAG
 ./autogen.sh
 ./configure --with-gui=no --without-miniupnpc --disable-wallet
 gmake
@@ -30,20 +42,28 @@ https://raw.githubusercontent.com/teran-mckinney/bitnoder/master/fs/usr/local/bi
 -o /usr/local/bin/honeybadgermoneystats
 
 # 2GiB node for testnet, 4GiB node for main
-if [ "$(sysctl -n hw.realmem)" -gt 4000000000 ]; then
-	echo > /usr/local/etc/bitnoder.conf
-else
-	echo 'TESTNET=1' > /usr/local/etc/bitnoder.conf
-fi
+# All testnet, actually.
+#if [ "$(sysctl -n hw.realmem)" -gt 4000000000 ]; then
+#echo > /usr/local/etc/bitnoder.conf
+#else
 
+echo 'TESTNET=1' > /usr/local/etc/bitnoder.conf
+
+#fi
+
+## This is not working out as planned...
 # This is a slow DD for a reason.
-dd if=/dev/zero bs=10M count=400 of=/var/tmp/swap
-mkswap /var/tmp/swap
-echo 'md99	none	swap	sw,file=/var/tmp/swap	0	0' >> /etc/fstab
+#dd if=/dev/zero bs=10M count=400 of=/var/tmp/swap
+#mkswap /var/tmp/swap
+#echo 'md99	none	swap	sw,file=/var/tmp/swap	0	0' >> /etc/fstab
 # Should look into swap blocksize down the road
-swapon -a
+#swapon -a
+
+echo 'ntpd_enable="YES"' >> /etc/rc.conf
+service ntpd start
 
 chmod 500 /etc/rc.local
 chmod 500 /usr/local/bin/honeybadgermoneystats
 
-/etc/rc.local
+# Let the boot process start rc.local on its own.
+#/etc/rc.local
